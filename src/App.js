1494146d664f3dd7d797f63ca7b01c6b0155be59
albertoconcovia <concovia@gmail.com>
2021-11-29 17:07:29 +0000

    useEffect and useState added into APP.js file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import './App.css';
+import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
+  // add styling later
+  // strip out excess boiler plate .
+  // consider waiting for your data to load (Hint: async and await)
+  // Remember to commit
+  // Add a read me explaining how to run your app when it’s done!
+  // Remember to check the documentation regularly
 
-// add styling later
-// strip out excess boiler plate .
-// consider waiting for your data to load (Hint: async and await)
-// Remember to commit 
-// Add a read me explaining how to run your app when it’s done!
-// Remember to check the documentation regularly
+  // App component but you are more than welcome to create another component if you like!
+  // Once your App is calling the Studio Ghibli API and displaying the first film title on your page ('Castle in the Sky'),
+  // add a test to mock (or “stub”) the API response.
+  // check the first film title has been rendered correctly (Hint: 'Castle in the Sky' should be in the document).
 
-// App component but you are more than welcome to create another component if you like!
-// Once your App is calling the Studio Ghibli API and displaying the first film title on your page ('Castle in the Sky'), 
-// add a test to mock (or “stub”) the API response. 
-// check the first film title has been rendered correctly (Hint: 'Castle in the Sky' should be in the document).
+  const [films, setFilms] = useState([]);
+  //display the title of the first film  ('Castle in the Sky').
+  const getFilms = async () => {
+    const apiResponse = await axios.get(
+      `https://ghibliapi.herokuapp.com/films/`
+    );
+    setFilms(apiResponse.data.data);
+  };
 
-// useEffect Hook for this
-const [films, setFilms] = useState([]);
-// Studio Ghibli API docs: https://ghibliapi.herokuapp.com/
-// make a GET request to the Studio Ghibli films end point and for your App component to display the title of the first film that comes back from the API end point ('Castle in the Sky').
-const getFilms = async (pageNumber) => {
-  const apiResponse = await axios.get(`https://ghibliapi.herokuapp.com/films/`);
-  setFilms(apiResponse.data.data);
-};
+  useEffect(() => {
+    console.log(`useEffect here`);
+  }, [films]);
 
   return (
-    <div >
-      <header >
-        <p>
-          Films from Studio Ghibli API
-        </p>
+    <div>
+      <header>
+        <p>Films from Studio Ghibli API</p>
       </header>
     </div>
   );
